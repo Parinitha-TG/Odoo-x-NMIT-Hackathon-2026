c353f17004e4f963e6093c801aa564f8c43cf71b
Priyadharshini <priyadh.0306@gmail.com>
2026-08-22 11:36:26 +0530

    Implement authentication registration validation

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+
+const authRoutes = require("./routes/authRoutes");
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+// Health check
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Dayflow backend is running"
+    });
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Dayflow backend running on port ${PORT}`);
+});
